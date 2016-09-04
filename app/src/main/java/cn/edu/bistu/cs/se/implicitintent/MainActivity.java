53fc8fb0ed34df307baac6368dfb6d01a71be360
ZhangXinJie6 <211088842@qq.com>
2016-09-04 23:55:26 +0800

    Initial commit

---
+++ app/src/main/java/cn/edu/bistu/cs/se/implicitintent/MainActivity.java
@@ -0,0 +1,24 @@
+package cn.edu.bistu.cs.se.implicitintent;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button button=(Button)findViewById(R.id.ButtonAnother);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent();
+                intent.setAction("startAnotherActivity");
+                startActivity(intent);
+            }
+        });
+    }
+}
